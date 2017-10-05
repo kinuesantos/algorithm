cb10428cedc63717cc4bfb333a1d15e5b10f613d
Kinue Santos <kinue@llip.life>
2017-10-05 16:44:36 -0700

    challenge2

---
+++ biggestNum/biggestnum.js
@@ -0,0 +1,13 @@
+const biggest=(num1,num2,num3)=>{
+  if(num1>num2 && num1>num3){
+    return num1;
+  }
+  if(num2>num1 && num2>num3){
+    return num2;
+  }
+  if(num3>num1 && num3>num2){
+    return num3;
+  }
+}
+
+module.exports= biggest;
