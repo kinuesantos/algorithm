5c4821328e44ba4335d58994d110f3a35cce22bd
Kinue Santos <kinue@llip.life>
2017-10-09 18:45:10 -0700

    solved 5

--- solutions/5.js
+++ solutions/5.js
@@ -1,8 +1,7 @@
 const sayHi = (func,num, i=0)=>{
-  if(i==num){
-    return;
-  }
-  func();
-  return sayHi(func,num,i+1);
+  if(i<num){
+    func();
+    return sayHi(func,num,i+1);
+}
 }
 module.exports= sayHi;
