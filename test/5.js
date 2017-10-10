1636ce48aff65c34946ce9ff09015aa323982249
Kinue Santos <kinue@llip.life>
2017-10-09 17:56:16 -0700

    solved 5

--- test/5.js
+++ test/5.js
@@ -1,36 +1,17 @@
 const sayHi= require('../solutions/5.js');
 
-let sum = 0;
-const greet = ()=>{
-  sum = sum + 1;
+const testing=(num)=>{
+  let sum = 0;
+  const greet = ()=>{
+    sum = sum + 1;
+  }
+  sayHi(greet,num);
+  if(sum==num){
+    console.log('You are right');
+  }else{
+    console.log('wrong')
+  }
 }
+testing(3);
+testing(10);
 
-
-
-//first test
-sayHi(greet,3);
-
-if(sum==3){
-  console.log('correct');
-}else{
-  console.log('wrong');
-}
-
-//second test
-sum = 0;
-sayHi(greet,20);
-if(sum==20){
-  console.log('correct');
-}else{
-  console.log('wrong');
-}
-
-//third test
-
-sum = 0;
-sayHi(greet,10);
-if(sum==10){
-  console.log('corerct');
-}else{
-  console.log('wrong');
-}
