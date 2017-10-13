b90e8096e94a4737a25fe175ec760d97c9cda69b
Kinue Santos <kinue@llip.life>
2017-10-12 21:08:23 -0700

    solved 10.js

--- solutions/10.js
+++ solutions/10.js
@@ -1,13 +1,20 @@
-const callFuncBig = (func,num1, num2,biggest, i=0)=>{
+const hello=()=>{
+  console.log('kinue');
+}
+
+const callFuncBig = (func,num1, num2,biggest, i=1)=>{
+  if(num1<0 &&num2<0){
+    return;
+  }
   if(num1>num2){
     biggest= num1;
   }else{
   biggest = num2;
   }
   if(i==biggest){
-    return i;
+    return;
   }
-  func();
+  func(i);
   return callFuncBig(func,num1,num2,biggest,i+1);
 }
 module.exports= callFuncBig;
