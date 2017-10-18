79d604939ca2ebeb55201ec55e4f233c937fec91
Kinue Santos <kinue@llip.life>
2017-10-18 11:21:35 -0700

    solved lesson3

--- solutions/lesson3.js
+++ solutions/lesson3.js
@@ -5,7 +5,7 @@ const kebabCase=(cam,i=0,newStr=[])=>{
   if(cam[i]==cam[i].toLowerCase()){
     newStr.push(cam[i]);
   }else{
-    newStr.push("-" + cam[i].toLowerCase());    
+    newStr.push(`-${cam[i].toLowerCase()}`);    
   }
   return kebabCase(cam,i+1,newStr);
 }
