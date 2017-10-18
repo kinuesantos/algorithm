719a2244ea0d91db352cdd1309ff37e4b49639e7
Kinue Santos <kinue@llip.life>
2017-10-18 11:16:19 -0700

    solved lesson3.js

---
+++ solutions/lesson3.js
@@ -0,0 +1,12 @@
+const kebabCase=(cam,i=0,newStr=[])=>{
+  if(i==cam.length){
+    return newStr.join('');
+  }
+  if(cam[i]==cam[i].toLowerCase()){
+    newStr.push(cam[i]);
+  }else{
+    newStr.push("-" + cam[i].toLowerCase());    
+  }
+  return kebabCase(cam,i+1,newStr);
+}
+module.exports= kebabCase;
