7505a1c8001e30e353019216b685539b19cfaa83
Kinue Santos <kinue@llip.life>
2017-11-03 21:56:59 -0700

    solved map

---
+++ solutions/map.js
@@ -0,0 +1,10 @@
+const map=(obj,func,i=0,arrKeys=Object.keys(obj))=>{
+  if(arrKeys.length==i){ 
+    return;
+  }
+  key=arrKeys[i];
+  value=obj[arrKeys[i]];
+  func(key,value);
+  return map(obj,func,i+1,arrKeys);
+}
+module.exports=map;
