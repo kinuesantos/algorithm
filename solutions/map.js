d705e8d36a6f6569065fd83f3aeb66057d56ffbe
Kinue Santos <kinue@llip.life>
2017-11-07 13:19:00 -0800

    solved and modified map

--- solutions/map.js
+++ solutions/map.js
@@ -1,7 +1,5 @@
 const map=(obj,func,i=0,arrKeys=Object.keys(obj))=>{
-  if(arrKeys.length==i){ 
-    return;
-  }
+  if(arrKeys.length==i) return;
   key=arrKeys[i];
   value=obj[arrKeys[i]];
   func(key,value);
--- test/map.js
+++ test/map.js
@@ -3,22 +3,20 @@ const map= require('../solutions/map.js');
 obj1={name:'kinue',id:123,female:true, color:'blue', time:'7am'};
 obj2={hello:'hola',name:'nombre', age:'edad'}
 obj3={color:'blue', number:158, food:true, drink:'juice'}
-let count=0;
 
-const func=(key,value)=>{
-  count=count+1
-  console.log(`the property is ${key} and the respective value is ${value}`);
-}
 const testing=(obj,result)=>{
+  let count = 0;
+  const func=(key,value)=>{
+    count=count+1
+    console.log(`the property is ${key} and the respective value is ${value}`);
+  }
   map(obj,func);
   if(count==result){
-    console.log('correct')
+    console.log('correct');
   }else{
-    console.log('wrong')
+    console.log('wrong');
   }
 }
 testing(obj1,5);
-count=0;
 testing(obj2,3);
-count=0;
 testing(obj3,4);
